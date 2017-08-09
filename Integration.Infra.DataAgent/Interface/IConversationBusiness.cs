bd53c49f8724deef289faddc21e8910f058c0893
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-08-09 20:11:36 -0300

    Add project files.

---
+++ Integration.Infra.DataAgent/Interface/IConversationBusiness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration.Infra.DataAgent.Interface
+{
+    public interface IConversationBusiness
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="alias"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> GetEmailMappingAsync(String alias);
+    }
+}
---
+++ Integration.Infra.DataAgent/Interface/IOrderBusiness.cs
@@ -0,0 +1,71 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration.Infra.DataAgent.Interface
+{
+    public interface IOrderBusiness
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> GetOrder(string orderId);
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<HttpResponseMessage> ListOrders(string queries);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> GetOrderCancel(string orderId);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> PostRegisterChanges(string orderId, JsonObject payload);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> PostStartHandling(string orderId, JsonObject payload);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> PostChangeOrderState(string orderId);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> Interations(string orderId, JsonObject payload);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        Task<HttpResponseMessage> PostNote(string orderId, JsonObject payload);
+    }
+}
